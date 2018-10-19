ec98144d1b4db8b8feb5c022e397f016dfa83792
JustineCrenier <justine.crenier@gmail.com>
2018-10-19 13:24:32 +0200

    partenaires section

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,11 @@ var swiper2 = new Swiper('.projets', {
       loop: true,
       loopFillGroupWithBlank: true
   });
+var swiper3 = new Swiper('.partenaires', {
+	  speed: 400,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true
+  });
