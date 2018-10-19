8e962605799847d08c4df461a0aa7cbaf99528f9
JustineCrenier <justine.crenier@gmail.com>
2018-10-19 13:08:40 +0200

    partie projets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-var swiper = new Swiper('.swiper-container');
+var swiper = new Swiper('.swiper-container',{
+	speed: 400
+});
+var swiper2 = new Swiper('.projets', {
+	  speed: 400,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true
+  });
