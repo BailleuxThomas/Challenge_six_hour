b47cc938120680b6905ae6d5b4ab09277d173622
BailleuxThomas <thomasbailleux2@gmail.com>
2018-10-26 14:58:22 +0200

    Mise a jours

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.swiper-container',{
+	speed: 400
+});
+var swiper2 = new Swiper('.projets', {
+	  speed: 400,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true
+  });
+var swiper3 = new Swiper('.partenaires', {
+	  speed: 400,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true
+  });
